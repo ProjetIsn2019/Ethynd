f572dedca4f86f42a2b6381fba7cbdf7a88e4d3d
Sofiane Djerbi <46628754+Kugge@users.noreply.github.com>
2019-01-26 00:26:30 +0100

    Update jeu.py

--- jeu.py
+++ jeu.py
@@ -1,28 +1,35 @@
 # -*- coding: utf-8 -*-
 """Programme final
 + Boucle de jeu
+Auteurs: Sofiane Dorian Anthony
 """
-from fonctions import maps
+from fonctions import map
 import pygame
 
 pygame.init()  # Mettre en route pygame
 pygame.display.set_caption("Ethynd")  # Titre de la fenêtre
-# pygame.display.set_icon("images/icone.png")  # Icone
-ecran = pygame.display.set_mode((640, 480))  # Affichage de la fenêtre
-# 600 et 400: c'est la taille de la fenêtre (20 et 15 tuiles)
+icone = pygame.image.load("images/icone.png") # Je charge l'icone
+pygame.display.set_icon(icone)  # Je met en place l'icone
+
+ecran = pygame.display.set_mode((0, 0),             # (0, 0) = taille de écran
+                                pygame.FULLSCREEN)  # Plein écran
+# On affiche la fenêtre lors de la définition de "ecran".
+
 quitter = False # On ne veux pas quitter le jeu dès le départ :')
 map = maps.Map("maps/test.map") # Chargement de la map
-map.afficher(ecran) # On affiche la map
 
 while quitter != True:  # Tant que le joueur ne veux pas jouer
     """ Boucle de jeu
     Boucle de jeu: Gestion events, executée en boucle.
+    1 éxécution = 1 tick.
     """
-
+    ecran.fill(map.couleur_fond)  # On change la couleur de fond (gris)
+    map.afficher(ecran) # On affiche la map chaque tick pour actaliser
+                        # les déplacements
     for event in pygame.event.get():  # pygame.event.get() = liste des events
         if event.type == pygame.KEYDOWN:  # Event : Touche enclenchée
             if event.key == pygame.K_ESCAPE:  # Touche = Echappe
                 quitter = True  # On quitte
 
-    pygame.display.flip()  # On change de frame. On actualise l'écran.
+    pygame.display.update()  # On change de tick. On actualise l'écran.
 pygame.quit()  # Quitter pygame.
