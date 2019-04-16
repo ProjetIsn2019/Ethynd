dec4d00f78a03bfd554d0b89add8eceb972d51ad
SofianeDjerbi <sofiane.djerbi38@gmail.com>
2019-04-16 11:12:01 +0200

    Bugfix

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ Auteurs: Sofiane Dorian Anthony
 import pygame as pg
 from fonctions import jeu
 
-pg.mixer.pre_init(44100, -16, 2, 1024)  # Réglages du mixeur pygame (fréquence (Hz), nombre de bits )
+pg.mixer.pre_init(44100, -16, 2, 1024)  # Réglages du mixeur pygame (fréquence (Hz), nombre de bits, channel, taille du buffer)
 pg.mixer.init()  # Initialisation du mixeur audio pygame
 pg.init()  # Mettre en route pygame
 jeu.initialiser_fenetre()  # Créer et initialiser la fenêtre
