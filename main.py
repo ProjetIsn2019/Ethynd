a66782c5cf6fe7d8018f900f52b612b13fca8ddd
Voland Dorian <46784712+CerfVoland@users.noreply.github.com>
2019-04-24 10:27:38 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*--son
 """Programme final
 Auteurs: Sofiane Dorian Anthony
 """
@@ -7,8 +7,9 @@ from fonctions import jeu
 
 pg.mixer.pre_init(44100, -16, 2, 1024)  # Réglages du mixeur pygame (fréquence (Hz), nombre de bits, channel, taille du buffer)
 pg.mixer.init()  # Initialisation du mixeur audio pygame
+pg.mixer.set_num_channels(8)
 pg.init()  # Mettre en route pygame
 jeu.initialiser_fenetre()  # Créer et initialiser la fenêtre
 jeu.musique()
 jeu.menu()  # Affichage du menu
-pg.quit()  # Quitter pg.
+pjg.quit()  # Quitter pg.
